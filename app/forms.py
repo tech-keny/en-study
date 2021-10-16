74c83504aefc522a45f317fd1a00d3f7d1cd537c
tech-keny <chanken7@icloud.com>
2021-10-16 10:19:56 +0900

    forms.py 修正

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from .models import Comment, Level, Post
+# from .models import Comment, Level, Post
 
 
 class PostForm(forms.Form):
-    level_data = Level.objects.all()
-    level_choice = {}
-    for level in level_data:
-        level_choice[level] = level
+    # level_data = Level.objects.all()
+    # level_choice = {}
+    # for level in level_data:
+    #     level_choice[level] = level
     title = forms.CharField(max_length=200, label='タイトル')
     study_time = forms.IntegerField(label="平均勉強時間/日")
-    level = forms.ChoiceField(label='レベル', widget=forms.Select, choices=list(level_choice.items())) # 追加
+    # level = forms.ChoiceField(label='レベル', widget=forms.Select, choices=list(level_choice.items())) # 追加
     textbook = forms.CharField(label="参考書名",max_length=200)
     image = forms.ImageField(label="参考書表紙", required=False)
     content = forms.CharField(label='勉強法', widget=forms.Textarea())
